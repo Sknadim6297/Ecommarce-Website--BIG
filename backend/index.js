5c569ac1cb0d7f95efa8065cf77f6929d60bb138
SK NADIM <128034110+Sknadim6297@users.noreply.github.com>
2024-08-24 16:49:53 +0530

    last commit2

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,10 @@ dotenv.config();
 
 app.use(cors({
   origin: process.env.FRONTEND_URL, 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   credentials: true 
 }));
+
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.json({ limit: "10mb" }));
